6a3ed605de7146898a4f8c396abab0c08b89b4bc
OlayG <ogureje@yahoo.com>
2016-07-04 22:41:34 -0400

    Added comments

--- PrimePalindrome/Program.cs
+++ PrimePalindrome/Program.cs
@@ -32,6 +32,7 @@ namespace PrimePalindrome
             Console.ReadKey();
         }
 
+        // Prints list of calculated palindromes
         private static void PrintPalindromeList()
         {
             Console.WriteLine("\nThese are the list of prime palindromes");
@@ -40,56 +41,67 @@ namespace PrimePalindrome
                 Console.Write("{0} ",n);
             }
         }
-
+        // Finds the prime palindrome fromt he list of prime numbers
         private static void CalculatePrimePalindrome()
         {
+            // Loops through all prime numbers in list
             foreach (int n in PrimeNumbers)
             {
+                // If number is a prime palindrome add it to list
                 if (isPrimePalindrome(Convert.ToString(n)))
                 {
                     primePalindrome.Add(Convert.ToString(n));
                 }
             }
         }
-
+        // Checks if the prime number is a prime palindrome
         private static bool isPrimePalindrome(string v)
         {
+            // If the first index is equall to last index return true
             if (v.First() == v.Last())
             {
                 return true;
             }
             return false;
         }
-
+        // Gets the list of prime number form number provided by user
         private static void getPrimeNumbers(int n)
         {
-            int counter = 0;
+            int counter;
 
+            // Loop to run through the numbers and find if it is prime
             for(int i = 1; i <= n; i++)
             {
+                // Gets the square root of each number
                 int numsToCheck = (int)Math.Sqrt(i);
+                // Reset counter to 0
                 counter = 0;
 
+                // Loop to the result of square root of number
                 for (int j = 1; j <= numsToCheck; j++)
                 {
+                    // If any number in the loop is equal to the number determined by outter loop this runs
                     if (i % j == 0)
                     {
                         counter++;
-
+                        // This is prime rule 
                         if (i % i == 0)
                             counter++;
                     }
 
                 }
 
+                // If counter is > 2 which means its divisible by more than 2 numbers 
+                // and i is not 1 which is a prime rule this runs
                 if (counter > 0 && counter < 3 && i != 1)
                 {
+                    // Adds value to the prime number list
                     PrimeNumbers.Add(i);
                 } 
 
             }
         }
-
+        // Prints out all prime numbers in list
         private static void printPrimeNumbers()
         {
             Console.WriteLine("\nHere are the list of prime numbers...");
